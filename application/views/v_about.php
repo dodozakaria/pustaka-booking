1a37c62d94a155defed29be62094f863635b61f4
paudislamuna <paudislamuna02@gmail.com>
2022-11-02 19:30:10 +0700

    tugas pert 7

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan    : Widodo Zakaria</li>
+        <li>Nama Belakang : Sumardi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Desa Luwijawa Rt.10 Rw.02 Kec. Jatinegara Kab. Tegal</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tegal</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Volly</li>
+     </ul>
+</section>
